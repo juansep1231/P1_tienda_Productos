9b90ef0dc9237734700f28d76de5ba9d6d3aafc0
juanse <juan.posso@epn.edu.ec>
2022-11-28 12:33:26 -0500

    fix: Se añade la funcionalidad para que no se abra cerrarsesion si no existe una sesion

--- cerrarsesion.php
+++ cerrarsesion.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+if(!isset($_SESSION["sn_usuario"])&&!isset($_SESSION["sn_clave"])){
+    header("Location: index.php");
+}
 session_destroy();
 header("Location: index.php");
 
